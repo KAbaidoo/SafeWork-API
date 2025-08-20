ef8c78ed21cb48f3aa3f269c7997398ea95965b8
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-08-20 06:39:34 +0000

    Initial commit

---
+++ src/main/java/com/safework/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.safework.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
